07f4683b20af6a0b67d2d9ea0f28b0d3b68d5fc5
ngt1986 <ngt1986@gmail.com>
2021-01-24 12:11:48 -0600

    updated 24-jan-2021

--- Clash Royale Clan Manager/clan_stats_update.py
+++ Clash Royale Clan Manager/clan_stats_update.py
@@ -20,14 +20,39 @@ master_file = 'clash_nasty.xlsx'
 date = datetime.today().strftime('%Y-%m-%d')
 csv_file = clan_tag + '_' + date + '.csv'
 
+king_levels_xpath = "/html/body/div[1]/article/main/div[4]/section[1]/div/div[2]/div[1]/section[1]/label[2]/div"
+last_seen_xpath = "/html/body/div[1]/article/main/div[4]/section[1]/div/div[2]/div[1]/section[1]/label[3]/div"
+
+king_levels_css = "#content > main > div:nth-child(4) > section.col-7.col-m-7 > div > div.module-body > div.row.checkbox-set > section:nth-child(1) > label(2)"
+last_seen_css = "#content > main > div:nth-child(4) > section.col-7.col-m-7 > div > div.module-body > div.row.checkbox-set > section:nth-child(1) > label(3)"
+
+checkbox_set_css = "#content > main > div:nth-child(4) > section.col-7.col-m-7 > div > div.module-body > div.row.checkbox-set > section:nth-child(1)"
+
 print('configuring web driver...')
 op = webdriver.ChromeOptions()
 op.add_argument('headless')
+op.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=op)
 params = {'behavior': 'allow', 'downloadPath': r'C:\Users\nthomas3\Python\Projects\Clash Royale Clan Manager'}
 driver.execute_cdp_cmd('Page.setDownloadBehavior', params)
 driver.get("http://pixelcrux.com/Clash_Royale/Clan_Manager/App?tag="+clan_tag)
 
+time.sleep(3)
+
+print('checking that all data checkboxes are checked...')
+
+checkboxes_span = driver.find_elements_by_class_name("checkbox-text")
+checkboxes_input = driver.find_elements_by_css_selector("input[type='checkbox']")
+#there is a mismatch in the number of checkboxes found by "checkbox-text", which is 6, and input[type='checkbox'], which is 9
+for box in checkboxes_input[3::]:
+    #print(box.is_selected())
+    if box.is_selected():
+        print('checkbox ' + str(checkboxes_input.index(box) - 3) + ' is checked')
+    else:
+        print('checkbox ' + str(checkboxes_input.index(box) - 3) + ' is not checked, checking box ' + str(checkboxes_input.index(box) - 3))
+        checkboxes_span[checkboxes_input.index(box)-3].click()
+
+    
 
 print('attempting to download ' + csv_file + '...')
 element = driver.find_element_by_xpath(xpath_csv)
@@ -39,6 +64,7 @@ element = driver.find_element_by_xpath(xpath_csv)
 
 element.click()
 
+# wait for download complete
 time.sleep(5)
 print('closing web driver...')
 driver.quit()
@@ -52,5 +78,6 @@ writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 pd.read_csv(csv_file).to_excel(writer, sheet_name = date)
 
 book.font = fontStyle
-writer.save()
+book.save(master_file)
 writer.close()
+print('done!')
